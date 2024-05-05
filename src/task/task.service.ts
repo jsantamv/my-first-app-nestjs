1ef19f7c5afc85685d7e7786de42638d7a843958
Juan Carlos Santamaria V <jsantamv@gmail.com>
2024-05-04 19:12:18 -0600

    update

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TaskService {
     getTask() {
-        return ['task1','task4','task3','task2']
+        return [
+            {
+                "dato":1,
+                "llave":"llave"
+            },
+            {
+                "dato":2,
+                "llave":"llave2"
+            }
+        ]
     }
 }
